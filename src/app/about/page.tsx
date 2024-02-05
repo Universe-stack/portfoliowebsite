fe11b7202ec7a0ef92f22285c0cac377ab06026d
Kachi <justiceChinedu156@gmail.com>
2024-02-05 19:48:56 +0100

    changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,62 +1,24 @@
-"use client"
-import { useState } from "react";
-import Image from "next/image";
-import dashboard from "../../../public/dashboard.jpg";
-import saas from "../../../public/saas.jpg";
-import reals from "../../../public/realsScreenshot.png";
-import suits from "../../../public/suitsAndVeil.png";
-import Link from "next/link";
-import TextAnimation from "../components/UI/TextAnimation";
+import React from 'react'
 
-export default function About() {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
+type Props = {}
 
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredIndex(null);
-  };
-
-  const projects = [
-    { image: dashboard, description: "Bookkeeping Software" },
-    { image: saas, description: "Custom SaaS App" },
-    { image: reals, description: "Custom SaaS App" },
-    { image: suits, description: "Suits" },
-  ];
-
+const page = (props: Props) => {
   return (
-    <section className="flex justify-center align-middle h-[90vh]">
-      <div className="self-center flex w-[75%] gap-[1.5rem] p-[1.5rem] h-[100%]">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className="w-[25%] h-[15rem] self-center relative cursor-pointer transform transition-transform duration-300 ease-in-out hover:translate-y-[-10px] hover:scale-105"
-            onMouseEnter={() => handleMouseEnter(index)}
-            onMouseLeave={handleMouseLeave}
-          >
-            <div className="w-[100%] h-[100%] overflow-hidden">
-              <Image
-                src={project.image}
-                alt="logo"
-                className="self-center contain h-[100%] w-[100%] grayscale hover:pointer"
-              />
-            </div>
-            {hoveredIndex === index && (
-              <div className="absolute top-0 bottom-0 right-0 left-0 bg-gray-600 opacity-80 text-white flex flex-col justify-center items-center transition-opacity duration-300 ease-in-out opacity-0">
-                <p className="text-center">{project.description}</p>
-                <span className>
-                  
-                </span>
-              </div>
-            )}
-            <p className="font-[400] mt-[0.9rem] text-[1rem]">
-              {hoveredIndex === index && <TextAnimation text={project.description} />}
+    <section className='w-[100%] flex flex-col justify-center align-middle '>
+        <div className='w-[55%] self-center flex flex-col gap-[1rem] mt-[4rem] p-[1.5rem]'>
+            <p>
+                <span className='font-[600] text-xl'>Onyekachi Justice Chinedu</span> (born 17 February 1991) is an English singer-songwriter. Born in Halifax, West Yorkshire, and raised in Framlingham, Suffolk, he began writing songs around the age of eleven. In early 2011, Sheeran independently released the extended play No. 5 Collaborations Project. He signed with Asylum Records the same year.
             </p>
-          </div>
-        ))}
-      </div>
+            <p >
+                Sheeran's debut album, + ("Plus"), was released in September 2011 and topped the UK Albums Chart. It contained his first hit single "The A Team". In 2012, Sheeran won the Brit Awards for Best British Male Solo Artist and British Breakthrough Act. Sheeran's second studio album, × ("Multiply"), topped charts around the world upon its release in June 2014. It was named the second-best-selling album worldwide of 2015. In the same year, × won Album of the Year at the 2015 Brit Awards, and he received the Ivor Novello Award for Songwriter of the Year from the British Academy of Songwriters, Composers and Authors. A single from ×, "Thinking Out Loud", earned him the 2016 Grammy Awards for Song of the Year and Best Pop Solo Performance.
+            </p>
+            <p>
+                Sheerans third album, ÷ ("Divide"), was released in March 2017, and was the best-selling album worldwide of 2017. The first two singles from the album, "Shape of You" and "Castle on the Hill", broke records in a number of countries by debuting in the top two positions of the charts. He also became the first artist to have two songs debut in the US top 10 in the same week. By March 2017, Sheeran had accumulated ten top-10 singles from ÷ on the UK Singles Chart, breaking the record for most top-10 UK singles from one album. His fourth single from ÷, "Perfect", reached number one in the US, Australia, and the UK, where it became the Christmas number one in 2017. The world's best-selling artist of 2017, he was named the Global Recording Artist of the Year. Released in 2019, his fourth overall and first collaborative album, No.6 Collaborations Project, debuted at number one in most major markets, and spawned three UK number one singles, "I Don't Care", "Beautiful People", and "Take Me Back to London". His fifth studio album, = ("Equals"), topped the charts in most major markets in 2021. His sixth album, - ("Subtract"), was released on 5 May 2023, while his seventh album, Autumn Variations, was released on 29 September 2023 under his own record label, Gingerbread Man Records.
+            </p>
+        </div>
     </section>
-  );
+  )
+
 }
+
+export default page
